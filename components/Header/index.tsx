25c10f3d2772b4f5b36905232f8dda30eb72cb39
tanvohoang <128999434+tanvohoang@users.noreply.github.com>
2023-06-26 11:53:37 +0700

    theme error on switching dark theme

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -76,7 +76,7 @@ const Header = () => {
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="/images/logo/logo.png"
                   alt="logo"
                   width={140}
                   height={30}
